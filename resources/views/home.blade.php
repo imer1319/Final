38ffce9d4809a98469dd9acdfa636eba40c2440e
ImerMamaniPerez <imerSabio24@gmail.com>
2019-08-24 14:30:41 -0400

    sistema con la ventana de home complementando

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-lg-4 col-sm-4 ">
+            <div>
+                <img src="{{ asset('imagenes/lecciones.png') }}" 
+                style="height: 200px; width: 200px;" alt="Card image cap">
+            </div>
+            <div class="text-center">
+                Lecciones
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <div>
+                <img src="{{ asset('imagenes/examen.png') }}" 
+                style="height: 200px; width: 200px;" alt="Card image cap">
+            </div>
+            <div class="text-center">
+                Examen
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <div>
+                <img src="{{ asset('imagenes/ejercicios.png') }}" 
+                style="height: 200px; width: 200px;" alt="Card image cap">
+            </div>
+            <div class="text-center">
+                Ejercicios
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <div>
+                <img src="{{ asset('imagenes/puntuaciones.png') }}" 
+                style="height: 200px; width: 200px;" alt="Card image cap">
+            </div>
+            <div class="text-center">
+                Puntuaciones
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <div>
+                <img src="{{ asset('imagenes/posicion.png') }}" 
+                style="height: 200px; width: 200px;" alt="Card image cap">
+            </div>
+            <div class="text-center">
+                Posicion
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <div>
+                <img src="{{ asset('imagenes/premios.png') }}" 
+                style="height: 200px; width: 200px;" alt="Card image cap">
+            </div>
+            <div class="text-center">
+                Premios
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
